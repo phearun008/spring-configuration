ef3a8033b976205a69211415e236021ff0c4b67f
phearun008 <PC1@192.168.178.71>
2016-08-04 11:07:14 +0700

    first-commit

---
+++ spring-configuration/src/main/java/error/SpringConfigurationApplication.java
@@ -0,0 +1,12 @@
+package error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigurationApplication.class, args);
+	}
+}
---
+++ spring-configuration/src/test/java/error/SpringConfigurationApplicationTests.java
@@ -0,0 +1,16 @@
+package error;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringConfigurationApplication.class)
+public class SpringConfigurationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
